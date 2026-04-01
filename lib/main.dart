47f0c6cdf25348eae27dfbf41d90fdcf8acbfd6c
Eric R Sant ana <168548989+rcxzz@users.noreply.github.com>
2026-04-01 10:48:40 -0300

    invertexto

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:invertexto/view/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+}
