c47ca5e4c3825cedfe1af9f445c286bd7b5949d1
Eric R Sant ana <168548989+rcxzz@users.noreply.github.com>
2026-04-01 10:50:38 -0300

    Delete lib directory

--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:invertexto/view/home_page.dart';
-
-void main() {
-  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
-}
